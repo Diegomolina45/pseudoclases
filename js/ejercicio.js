d2ffe8a93c94d66bc0f78b107325a2db4413a169
Diego Molina <molina542001@gmail.com>
2021-06-22 12:44:10 -0500

    cambios del 60% restante

--- js/ejercicio.js
+++ js/ejercicio.js
@@ -10,3 +10,6 @@ let output = document.getElementById("output");
 if (paragraph.matches(":scope")) {
   output.innerText = "Yep, the element is its own scope as expected!";
 }
+document.querySelector(".print").onclick = function () {
+    window.print();
+  }
